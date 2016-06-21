63db3499c9f08f1cd2e4c4f11579950e0cb856c9
RichoM <richi.moran@gmail.com>
2016-06-21 12:26:41 -0300

    Implemented a simple (and invalid, for now) translation for each ast node.

--- parser/compiler.js
+++ parser/compiler.js
@@ -3,13 +3,85 @@ var compiler = (function (parser) {
 	
 	function compile(string) {
 		var ast = parser.parse(string);
-		for (var i = 0; i < ast.length; i++) {
-			var expr = ast[i];
-			console.log(expr.type);
+		var js = ast.map(visit).join("\n");
+		return js;
+	}
+	
+	function visit(expr) {
+		// TODO(Richo): Maybe replace with an actual visitor...
+		switch (expr.type) {
+			case "Variable": 
+				return visitVariable(expr); 
+				break;
+			case "Number": 
+				return visitNumber(expr); 
+				break;
+			case "String": 
+				return visitString(expr); 
+				break;
+			case "Assignment": 
+				return visitAssignment(expr); 
+				break;
+			case "Array": 
+				return visitArray(expr); 
+				break;
+			case "MessageSend": 
+				return visitMessageSend(expr); 
+				break;
+			case "Method": 
+				return visitMethod(expr); 
+				break;
+			case "Javascript": 
+				return visitJavascript(expr); 
+				break;
 		}
 	}
 	
+	function visitVariable(expr) {
+		return expr.value;
+	}
+	
+	function visitNumber(expr) {
+		return expr.value;
+	}
+	
+	function visitString(expr) {
+		return "'" + expr.value + "'";
+	}
+	
+	function visitAssignment(expr) {
+		return visit(expr.left) + " = " + visit(expr.right);
+	}
+	
+	function visitArray(expr) {
+		return "[" + expr.elements.map(visit).join(", ") + "]";
+	}
+	
+	function visitMessageSend(expr) {
+		return visit(expr.rcvr) + "." + 
+			expr.selector.replace(/:/g, "_") + "(" +
+			expr.args.map(visit).join(", ") + ")";
+	}
+	
+	function visitMethod(expr) {
+		return "(function " + expr.selector.replace(/:/g, "_") + "(" +
+			expr.args.join(", ") + ") {" +
+			expr.temps.map(function (tmp) { 
+				return "var " + tmp + ";";
+			}).join(" ") +
+			expr.body.map(visit).join("; ")	+ "})"
+	}
+	
+	function visitJavascript(expr) {
+		return expr.code;
+	}
+	
+	function evaluate(string) {
+		return compiler.compile(string);
+	}
+	
 	return {
-		compile: compile
+		compile: compile,
+		evaluate: evaluate
 	};
 })(parser);

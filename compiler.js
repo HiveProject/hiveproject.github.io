77007fd439de5aa8561de169d6cb65c2d6f980b0
Matias Teragni <matias.teragni@gmail.com>
2016-10-12 14:33:50 -0300

    Revert "fixed bug introduced in last commit"
    This reverts commit 8a5564ed8484758c26e2e9d63743d7ef3fb242da.

--- compiler.js
+++ compiler.js
@@ -70,17 +70,16 @@ var compiler = (function (parser) {
 
 	function visitMethod(expr) {
 		var body = expr.body.map(visit);
-		body.push("return ("+ body.pop()+")");
-		return `CreateMethod("` +  expr.selector+
-		`","`+  "("+"function " + "(" +
-			expr.args.join(", ") + ") {" +
-			expr.args.map(function(item){return "context.set('"+item+"',"+item+");";}).join("")+
-			expr.temps.map(function (tmp) { 
-				return "var " + tmp + ";";
-			}).join(" ") +
-			body.join("; ").replace(/\\/g,'\\\\').replace(/"/g,'\\"') + `; })",context)`
-	}
-	
+		body.push("return (" + body.pop() + ")");
+		return  ` CreateMethod("` +  expr.selector+
+					`", "`+  "("+" function " + "(" +
+							expr.args.join(", ") + ") {	" +
+							expr.args.map(function(item){return " context.set('"+item+"', "+item+");";}).join(" ")+
+							expr.temps.map(function (tmp) {
+								return " var " + tmp + ";";
+							}).join(" ") +
+							body.join(";").replace(/\\/g,'\\\\').replace(/" / g,'\\"') +  ` ;})",context)`
+				}
 
 	function visitJavascript(expr) {
 		return expr.code;

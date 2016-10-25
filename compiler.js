2559bf87ac915d508a979c9f71e8b730cf9f34b7
RichoM <richi.moran@gmail.com>
2016-10-25 16:34:36 -0300

    Fixed compilation of temps.

--- compiler.js
+++ compiler.js
@@ -73,9 +73,6 @@ var compiler = (function (parser) {
 			return 'CreateMethod("' + expr.selector + '", "(function (' +
 				expr.args.join(", ") + ") {" +
 				expr.args.map(function(item){return "context.set('"+item+"',"+item+");";}).join("")+
-				expr.temps.map(function (tmp) { 
-					return "var " + tmp + ";";
-				}).join(" ") +
 				expr.body.code.replace(/\\/g,'\\\\').replace(/"/g,'\\"') + '})", context)';
 		} else {			
 			var body = expr.body.map(visit);
@@ -85,7 +82,7 @@ var compiler = (function (parser) {
 				expr.args.join(", ") + ") {" +
 				expr.args.map(function(item){return "context.set('"+item+"',"+item+");";}).join("")+
 				expr.temps.map(function (tmp) { 
-					return "var " + tmp + ";";
+					return 'context.set("' + tmp + "', context.lookup('null').get());");
 				}).join(" ") +
 				body.join("; ").replace(/\\/g,'\\\\').replace(/"/g,'\\"') + `; })",context)`;
 		}

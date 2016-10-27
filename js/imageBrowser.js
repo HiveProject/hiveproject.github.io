7db9193d0b29789bdeb19184f9fc1c69155cd632
Matias Teragni <matias.teragni@gmail.com>
2016-10-27 15:42:37 -0300

    Removed Stars. Added google js library

--- js/imageBrowser.js
+++ js/imageBrowser.js
@@ -87,7 +87,6 @@
 						"<th>Owner</th>" +
 						"<th>File size</th>" +
 						"<th>Last modified</th>" +
-						"<th>Stars</th>" +
 					"</tr>" +
 				"</thead>");
 			resp.items.forEach(function (file) {
@@ -113,74 +112,13 @@
 						.append("<span>" + file.quotaBytesUsed + "</span>"));
 				row.append($("<td>")
 						.append("<span>" + file.modifiedDate + "</span>"));
-				appendStars(file, row);
 				
 				table.append(row)
 			});
 		 });
 	}
 	
-	function appendStars(file, row) {
-		var td = $("<td>");
-		var prop = undefined;
-		if (file.properties !== undefined) {
-			prop = file.properties.find(function (p) { return p.key === "stars"; });
-		}
-		var value = prop !== undefined ? prop.value : 0;
-		var stars = [];
-		for (var i = 0; i < 5; i++) {
-			(function (index) {
-				var star = $("<span>");
-				stars.push(star);
-				star.attr("class", "glyphicon glyphicon-star")
-					.css("color", index >= value ? "grey" : "goldenrod")
-					.on("click", function () {
-						stars.forEach(function (e, i) {
-							e.css("color", i > index ? "grey" : "goldenrod");
-						});
-						value = index + 1;
-						var request = gapi.client.drive.properties.insert({
-							'fileId': file.id,
-							'resource': {
-								'key': "stars",
-								'value': value,
-								'visibility': 'PRIVATE'
-							}
-						});
-						request.execute(function(resp) { console.log(resp); });
-						
-						// Let our server know
-						ajax.request({
-							type: 'POST',
-							url: '/events/classify',
-							data: {
-								document_id: file.id,
-								stars: value
-							},
-							success: function (resp) {								
-								console.log(resp);
-							},
-							error: function (err) {
-								console.log(err);
-							}
-						});
-					})
-					.on("mouseover", function () {
-						stars.forEach(function (e, i) {
-							e.css("color", i > index ? "grey" : "goldenrod");
-						});
-					})
-					.on("mouseout", function () {
-						stars.forEach(function (e, i) {
-							e.css("color", i >= value ? "grey" : "goldenrod");
-						});
-					});
-				td.append(star);
-			})(i);
-		}
-		row.append(td);
-	}
-
+	
 	function openFolder(id) {
 		var state = {
 			folder: id

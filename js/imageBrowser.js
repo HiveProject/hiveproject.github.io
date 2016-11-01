8870fe66afa3a3bffc5a51798a769016e4e3a3f9
Matias Teragni <matias.teragni@gmail.com>
2016-11-01 14:12:13 -0300

    I added a delay on the refresh of the page to give time to the saving process. This is a hack and should be done properly. Filtered the files shown by the browser.

--- js/imageBrowser.js
+++ js/imageBrowser.js
@@ -96,37 +96,40 @@
 					"</tr>" +
 				"</thead>");
 			resp.items.forEach(function (file) {
-				var row = $("<tr>");
-				row.append($("<td>")
-						.append($("<a>")
-							.attr("href", file.alternateLink)
-							.append($("<img>").attr("src", file.iconLink))
-							.append($("<span>").text(file.title)))
-							.on("click", function () {
-								if (file.mimeType === "application/vnd.google-apps.folder") {
-									// Open the folder inside the app
-									openFolder(file.id);
-									return false;
-								} else {
-									if(file.mimeType.startsWith(hiveMimeType)){
-										//i need to open the image. The real mimeType is something like "application/vnd.google-apps.drive-sdk.763227956228". Those numbers appear as soon as realtime initializes the file.
-										openImage(file.id);
+				if(file.mimeType.startsWith(hiveMimeType)){
+					var row = $("<tr>");
+					row.append($("<td>")
+							.append($("<a>")
+								.attr("href", file.alternateLink)
+								.append($("<img>").attr("src", file.iconLink))
+								.append($("<span>").text(file.title)))
+								.on("click", function () {
+									if (file.mimeType === "application/vnd.google-apps.folder") {
+										// Open the folder inside the app
+										openFolder(file.id);
 										return false;
 									} else {
-										// Let the browser handle it...
-										return true;
+										if(file.mimeType.startsWith(hiveMimeType)){
+											//i need to open the image. The real mimeType is something like "application/vnd.google-apps.drive-sdk.763227956228". Those numbers appear as soon as realtime initializes the file.
+											openImage(file.id);
+											return false;
+										} else {
+											// Let the browser handle it...
+											return true;
+										}
 									}
-								}
-							}));
-				row.append($("<td>")
-						.append("<span>" + file.ownerNames.join(", ") + "</span>"));
-				var sizeNode = $("<td>");
-				getBytes(file,function(size){sizeNode.append("<span>" + size + "</span>")});
-				row.append(sizeNode);						
-				row.append($("<td>")
-						.append("<span>" + file.modifiedDate + "</span>"));
-				row.append(appendActionButtons($("<td>"),file.id));
-				table.append(row)
+								}));
+					row.append($("<td>")
+							.append("<span>" + file.ownerNames.join(", ") + "</span>"));
+					var sizeNode = $("<td>");
+					getBytes(file,function(size){sizeNode.append("<span>" + size + "</span>")});
+					row.append(sizeNode);						
+					row.append($("<td>")
+							.append("<span>" + file.modifiedDate + "</span>"));
+					row.append(appendActionButtons($("<td>"),file.id));
+					table.append(row);
+				}
+					
 			});
 		 });
 	}
@@ -169,7 +172,10 @@
 					var originalModel=doc.getModel();
 					var newDoc = gapi.drive.realtime.loadFromJson(originalModel.toJson());
 					newDoc.saveAs(cres.id);
-					updateState([]);
+					console.log("Compressed from " + originalModel.bytesUsed + " To "+ newDoc.bytesUsed);
+					//the saveAs actually takes some time, and if while it happens i refresh the page then i have some issues because asking for weight 
+					//initializes the file and starts giving that annoying concurrent exception.
+					setTimeout(function(){updateState([])} , 2000); 
 				} );
 			});
 			

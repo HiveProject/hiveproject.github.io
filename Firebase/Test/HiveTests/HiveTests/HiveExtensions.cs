f58fec3718e9ab59d5fc1bcc1b3858e3bdde1d54
Matias Teragni <matias.teragni@gmail.com>
2018-05-04 12:52:11 -0300

    Added test project (visual studio core tests) to validate the functionality provided on js. This test project depends on chrome being installed on the computer, and controls it using the ChromeDevTools protocol.

---
+++ Firebase/Test/HiveTests/HiveTests/HiveExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tera.ChromeDevTools;
+
+namespace HiveTests
+{
+    public static class HiveExtensions
+    {
+        private const string hiveSetString = "hive.set(\"{0}\",{1})";
+        private const string hiveGetString = "hive.get(\"{0}\")";
+
+        public static async Task<List<ChromeSession>> CreateHiveSessions(this Chrome c, int amount)
+        {
+            List<ChromeSession> result = new List<ChromeSession>();
+
+            for (int i = 0; i < amount; i++)
+            {
+                var s = await c.CreateNewSession();
+                result.Add(s);
+                await s.Navigate("http://hiveproject.github.io/Firebase/");
+            }
+
+            return result;
+        }
+
+        public static async Task<T> hiveSetValue<T>(this ChromeSession s, string key, T value) where T : struct
+        {
+     //       var m = await s.EvalObject(string.Format(hiveSetString, key, value));
+
+            return await s.EvalValue<T>(string.Format(hiveSetString, key, value));
+        }
+        public static async Task<T> hiveGetValue<T>(this ChromeSession s, string key) where T : struct
+        {
+            return await s.EvalValue<T>(string.Format(hiveGetString, key));
+        }
+    }
+}
---
+++ Firebase/Test/HiveTests/HiveTests/ValueTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tera.ChromeDevTools;
+
+namespace HiveTests
+{ 
+    [TestClass]
+    public class ValueTests
+    {
+        Chrome chrome;
+        public ValueTests()
+        {
+            chrome = new Chrome(headless: false);
+        }
+        ~ValueTests()
+        {
+            chrome.Dispose();
+        }
+
+        [TestMethod]
+        public async Task NumberSyncTest()
+        {
+            List<ChromeSession> sessions =  await chrome.CreateHiveSessions(2);
+            await sessions[0].hiveSetValue("number", 5);
+            await Task.Delay(10);
+            Assert.AreEqual(5, await sessions[1].hiveGetValue<int>("number"), 0, "The value was not present on the second session!");
+        }
+
+    }
+
+}

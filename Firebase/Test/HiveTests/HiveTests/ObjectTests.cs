aad44ead85ef8b7d0db512addaf5cbd367348c08
Matias Teragni <matias.teragni@gmail.com>
2018-05-07 14:07:19 -0300

    Added first object sync test

---
+++ Firebase/Test/HiveTests/HiveTests/ObjectTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic; 
+using System.Text;
+using System.Threading.Tasks;
+using Tera.ChromeDevTools;
+
+namespace HiveTests
+{
+    [TestClass]
+    public class ObjectTests
+    {
+        Chrome chrome;
+        [TestInitialize]
+        public void Initialize()
+        {
+            chrome = new Chrome(headless: false);
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            chrome.Dispose();
+            chrome = null;
+        }
+
+        [TestMethod]
+        public async Task ObjectSyncTest()
+        {
+            List<ChromeSession> sessions = await chrome.CreateHiveSessions(2);
+            var p = new Point(10, 20);
+            await sessions[0].hiveSet("obj1", p);
+            await sessions[1].hiveWaitUntilGet("obj1",  5000);
+            Assert.IsTrue( await sessions[1].EvalValue<bool>("var p = hive.get(\"obj1\"); p.x ==10 &&p.y==20;" ),"the object was not correctly replicated");
+        }
+
+    }
+
+    public struct Point
+    {
+       public int x;
+       public int y; 
+
+        public Point(int x, int y) 
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
